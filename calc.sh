55015b588db175beeb328fd72e2bc62ddd192f63
daniellathomas <daniella.jasmine.t@gmail.com>
2021-11-10 19:08:00 -0500

    Made a calculator =)

--- calc.sh
+++ calc.sh
@@ -9,3 +9,56 @@
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+#!/bin/bash
+
+echo "Hello world! This is Daniella's calculator."
+echo ""
+
+while [ true ]
+do
+    clear;
+    echo "Please select one of the following options:"
+    echo "(A)DD" # A for ADD
+    echo "(S)UB" # S for SUB
+    echo "(M)ULT" # M for MULT
+    echo "(D)IV" # D for DIV
+    echo "(E)XIT" # E for EXIT
+    echo ""
+
+    # Read user input
+    read input
+
+    if [ "$input" == "A" ] || [ "$input" == "S" ] || [ "$input" == "M" ] || [ "$input" == "D" ] || [ "$input" == "E" ]
+    then
+        # If E then exit loop
+        if [ "$input" == "E" ]
+        then
+            echo "Bye Bye!";
+            break;
+        fi
+
+        # Get first number
+        read -p "Enter first number: " firstNumber;
+
+        # Get second number
+        read -p "Enter second number: " secondNumber;
+
+        case $input in
+            A) res=$(($firstNumber + $secondNumber));;
+            S) res=$(($firstNumber - $secondNumber));;
+            M) res=$(($firstNumber * $secondNumber));;
+            D) res=$(($firstNumber / $secondNumber));;
+        esac
+        echo "Result: $res";
+    echo ""
+
+    else
+        # Wrong selection
+        echo "Option entered is not valid."
+    echo ""
+    fi
+
+    #Holds the screen from clearing
+    echo "Press any key to continue"
+    read
+done
